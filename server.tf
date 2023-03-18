81eff12d52779264d7b2a15eeec9be1f6bdb840c
Chinonso Nwakudu <nwakudue@gmail.com>
2023-03-18 08:13:08 +0000

    update

--- server.tf
+++ server.tf
@@ -1,11 +1,11 @@
 # creating instance 1
 
 resource "aws_instance" "exam-server" {
-  ami             = "ami-0557a15b87f6559cf"
+  ami             = "ami-0aaa5410833273cfe"
   instance_type   = "t2.micro"
-  key_name        = "root-server2-london"
+  key_name        = "root-key"
   security_groups = [aws_security_group.exam-security-grp-rule.id]
-  subnet_id       = aws_subnet.exam-public-subnet1
+  subnet_id       = aws_subnet.exam-public-subnet1.id
   availability_zone = "eu-west-2a"
   tags = {
     Name   = "exam-server-1"
