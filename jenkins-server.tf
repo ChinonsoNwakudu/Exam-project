b6a7762003e18a8060f618026d7406727b0c5ae3
Chinonso Nwakudu <nwakudue@gmail.com>
2023-03-19 20:33:05 +0100

    first commit

--- jenkins-server.tf
+++ jenkins-server.tf
@@ -15,8 +15,8 @@ resource "aws_instance" "jenkins-server" {
   ami                         = data.aws_ami.latest-amazon-linux-image.id
   instance_type               = var.instance_type
   key_name                    = "root-key"
-  subnet_id                   = aws_subnet.myapp-subnet-1.id
-  vpc_security_group_ids      = [aws_default_security_group.default-sg.id]
+  subnet_id                   = aws_subnet.exam-public-subnet1.id
+  vpc_security_group_ids      = [aws_security_group.exam-security-grp-rule.id]
   availability_zone           = var.avail_zone
   associate_public_ip_address = true
   user_data                   = file("jenkins-server-script.sh")
