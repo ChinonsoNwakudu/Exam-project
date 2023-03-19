734c39f9be41c1d8b509503c04bafb8d0fb2132e
Chinonso Nwakudu <nwakudue@gmail.com>
2023-03-19 17:15:03 +0100

    update

--- main.tf
+++ main.tf
@@ -98,8 +98,8 @@ resource "aws_security_group" "exam-security-grp-rule" {
   vpc_id      = aws_vpc.exam_vpc.id
  ingress {
     description = "HTTP"
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
